397d9b17c9155d27795a87b9ee40cd1e20c19750
diatomic.ge <diatomic.ge@gmail.com>
2014-11-15 17:26:05 -0500

    Add copyright/license notice
    chanarch.py:
      Add copyright and GPLv3 license notice at the top

--- chanarch.py
+++ chanarch.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Copyright (C) 2014 diatomic.ge
+#
+# This file is part of chanarch.
+#
+# Chanarch is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of that License, or
+# (at your option) any later version.
+#
+# Chanarch is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with chanarch. If not, see <http://www.gnu.org/licenses/>
+
 # Compatibility
 from __future__ import division, print_function, unicode_literals
 
